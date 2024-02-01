73a538a51e7200aa3ce2d87dda222e8865425aee
manish <manishpatel.28598@gmail.com>
2024-02-02 00:33:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,340 @@
+let myjokes =[
+    {
+        "category": "Dark",
+        "type": "twopart",
+        "setup": "How do we know Jesus wasn't black?",
+        "delivery": "He would have been hung from a tree, not a cross.",
+        "flags": {
+            "nsfw": false,
+            "religious": true,
+            "political": false,
+            "racist": true,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 110,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Pun",
+        "type": "single",
+        "joke": "I have these weird muscle spasms in my gluteus maximus.\nI figured out from my doctor that everything was alright:\nHe said \"Weird flex, butt okay.\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 82,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Pun",
+        "type": "single",
+        "joke": "To whoever stole my copy of Microsoft Office, I will find you. You have my Word!",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 191,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Programming",
+        "type": "twopart",
+        "setup": "Why are Assembly programmers always soaking wet?",
+        "delivery": "They work below C-level.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": true,
+        "id": 264,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "twopart",
+        "setup": "What's the difference between a baby and a pile of sand?",
+        "delivery": "You can't pick a pile of sand up with a pitchfork.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": true
+        },
+        "id": 120,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Pun",
+        "type": "twopart",
+        "setup": "I hate Russian matryoshka dolls.",
+        "delivery": "They're so full of themselves.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 87,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Pun",
+        "type": "twopart",
+        "setup": "What did the cell say when his sister cell stepped on his foot?",
+        "delivery": "Mitosis.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 199,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Pun",
+        "type": "twopart",
+        "setup": "Which is faster, Hot or cold?",
+        "delivery": "Hot, because you can catch a cold",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": true,
+        "id": 279,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "twopart",
+        "setup": "What is the difference between a pizza and a black man?",
+        "delivery": "A pizza can feed a family of five.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": true,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 174,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Dark",
+        "type": "single",
+        "joke": "I was going to tell a dead baby joke. But I decided to abort.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": true
+        },
+        "id": 207,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "twopart",
+        "setup": "Why is every gender equality officer female?",
+        "delivery": "Because it's cheaper.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": true,
+            "explicit": false
+        },
+        "id": 61,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "twopart",
+        "setup": "Went to the doctors for a prostate exam.\nDuring the exam he said it's not unusual to become aroused or even ejaculate.",
+        "delivery": "But still, I wish he hadn't.",
+        "flags": {
+            "nsfw": true,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": true
+        },
+        "id": 83,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "twopart",
+        "setup": "I like my girls how I like my COVID.",
+        "delivery": "19 and easily spread.",
+        "flags": {
+            "nsfw": true,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": true
+        },
+        "id": 167,
+        "safe": false,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "single",
+        "joke": "My wife and I have reached the difficult decision that we do not want children.\nIf anybody does, please just send me your contact details and we can drop them off tomorrow.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 257,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "twopart",
+        "setup": "\"99.9% of the people are dumb!\"",
+        "delivery": "\"Fortunately I belong to the remaining 1%\"",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": true,
+        "id": 277,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "twopart",
+        "setup": "Why was the mushroom always invited to parties?",
+        "delivery": "Cause he's a fungi.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 223,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "twopart",
+        "setup": "What's the difference between a hot potato and a flying pig?",
+        "delivery": "One's a heated yam, the other's a yeeted ham.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 141,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "single",
+        "joke": "Relationship Status: just tried to reach for my dog's paw and he pulled it away so I pretended I was reaching for the remote.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "racist": false,
+            "sexist": false,
+            "political": false,
+            "explicit": false
+        },
+        "id": 195,
+        "safe": true,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "twopart",
+        "setup": "What's the difference between England and a tea bag?",
+        "delivery": "The tea bag stays in the cup longer.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "safe": true,
+        "id": 308,
+        "lang": "en"
+    },
+    {
+        "category": "Misc",
+        "type": "single",
+        "joke": "If you're here for the yodeling lesson, please form an orderly orderly orderly queue.",
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 139,
+        "safe": true,
+        "lang": "en"
+    }
+    
+    
+  ]
+          let jokes = Math.floor(Math.random() * myjokes.length - 1)
+          joke.innerHTML = myjokes[jokes].joke
